52c99a1b595469f9a8d9b3aa73140a704aa3a347
AlexJozwicki <contact@alexandrejozwicki.com>
2015-03-26 18:27:15 +0100

    Crude version of step-1

--- src/js/BasicReactSample.jsx
+++ src/js/BasicReactSample.jsx
@@ -5,12 +5,14 @@ var classnames  = require( 'classnames' );
 class BasicReactSample extends React.Component {
     constructor(props) {
         super( props );
+        this.state = { name: 'John Doe' };
     }
 
     render() {
         return (
             <div>
-                <h1>Welcome to React movie</h1>
+                <h1>Welcome {this.state.name} to React movie</h1>
+                <input type="text" value={this.state.name} onChange={( e, value ) => this.setState( { name: e.target.value } )}/>
             </div>
         );
     }
