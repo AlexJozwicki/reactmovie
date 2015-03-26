e0076e24f270e4b914de727e31e6c0388c259369
AlexJozwicki <contact@alexandrejozwicki.com>
2015-03-26 18:30:59 +0100

    Better solution

--- src/js/BasicReactSample.jsx
+++ src/js/BasicReactSample.jsx
@@ -5,14 +5,24 @@ var classnames  = require( 'classnames' );
 class BasicReactSample extends React.Component {
     constructor(props) {
         super( props );
-        this.state = { name: 'John Doe' };
+        this.state = { name: 'John Doe' };  // the state holds the dynamic data of your components
+    }
+
+    /**
+     * A function returning a ValueLink object is the recommanded way in React to bind inputs
+     */
+    linkState( key ) {
+        return {
+            value: this.state[ key ],
+            requestChange: ( value ) => this.setState( { [key]: value } )
+        };
     }
 
     render() {
         return (
             <div>
                 <h1>Welcome {this.state.name} to React movie</h1>
-                <input type="text" value={this.state.name} onChange={( e, value ) => this.setState( { name: e.target.value } )}/>
+                <input type="text" valueLink={this.linkState( 'name' )}/>
             </div>
         );
     }
